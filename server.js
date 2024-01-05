b519c1eb60b42c440ad0f4e633bb84411f458a10
thatguysilva <silva.unb.phys@gmail.com>
2024-01-04 21:05:57 -0300

    Update server configuration

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(serveStatic(path.join(__dirname, 'dist'), {'index': ['index.html', 'index.htm']}));
+// Serve the static files from the Vue app build
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+// Handle any requests that don't match the ones above by sending back Vue's index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port);
